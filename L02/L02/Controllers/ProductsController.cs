a7f35e406c1654c9a3065989e6d3bdaaf5c6e2c1
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-09-08 18:57:43 -0400

    Initial commit.

---
+++ L02/L02/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L02.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public string Index()
+        {
+            return "Products/Index is displayed.";
+        }
+
+        public string Browse()
+        {
+            return $"Browse displayed";
+        }
+
+        public string Details(int id)
+        {
+            return $"Details displayed for id={id}";
+        }
+
+        public string Location(string zip)
+        {
+            return HttpUtility.UrlEncode($"Products/Location?zip={zip}");
+        }
+    }
+}
