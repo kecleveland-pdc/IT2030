1b7353f156d929446cc346afdcbb73b18631f981
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-06 18:24:35 -0400

    Fixed DataFormatString for Price. Excluded AlbumID

--- L06/MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
+++ L06/MVCMusicStoreApplication/MVCMusicStoreApplication/Models/Album.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MVCMusicStoreApplication.Models
 {
+    [Bind(Exclude = "AlbumId")]
+
     public class Album
     {
         public virtual int AlbumId { get; set; }
@@ -19,13 +22,13 @@ namespace MVCMusicStoreApplication.Models
         [Display(Name ="Album Price")]
         [Required]
         [Range(0.01, 100.00,ErrorMessage="Album price must be between $0.01 and $100.00")]
-        [DisplayFormat(DataFormatString = "(0:c)")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
         public virtual decimal Price { get; set; }
 
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
 
-        [Display(Name = "Album ArtURL")]
+        [Display(Name = "Album Art URL")]
         [Required]
         public virtual string AlbumArtUrl {get;set;}
 
