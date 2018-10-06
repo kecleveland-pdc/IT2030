35f61e4e093c2bc5ecb13f999931725e67ae684e
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-06 19:55:03 -0400

    Added custom attribute class MinDateAttribute to validate date. Updated validation in Course, Enrollment, and Student classes.

--- L06/EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
+++ L06/EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -2,20 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace EnrollmentApplication.Models
 {
     public class Enrollment
     {
+        [DisplayName("Enrollment ID")]
         public virtual int EnrollmentID { get; set; }
+
         public virtual int StudentID { get; set; }
+
         public virtual int  CourseID { get; set; }
+
+        [Required]
+        [RegularExpression(@"\b[A-F]\b", ErrorMessage = "Grade must be A, B, C, D, E, F")]
         public virtual string Grade { get; set; }
+
         public virtual Student Student { get; set; }
+
         public virtual Course Course { get; set; }
+
         public virtual bool IsActive { get; set; }
+
+        [Required]
+        [DisplayName("Assigned Campus")]
         public virtual string AssignedCampus { get; set; }
+     
+        [Required]
+        [DisplayName("Enrolled in Semester")]
         public virtual string EnrollmentSemester { get; set; }
+
+        [Required]
+        [MinDate(2018)]
         public virtual int EnrollmentYear { get; set; }
     }
+
+    
 }
---
+++ L06/EnrollmentApplication/EnrollmentApplication/Models/MinDateAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
+{
+    
+    public class MinDateAttribute : ValidationAttribute
+    {
+        public MinDateAttribute(int year) : base("{0} must be at least 2018")
+        {
+            _year = year;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var year = Convert.ToInt32(value);
+                if (year < 2018)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        private readonly int _year;
+
+    }
+}
