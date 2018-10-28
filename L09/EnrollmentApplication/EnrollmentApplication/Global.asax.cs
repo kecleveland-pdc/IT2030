6d9bf6f0b2613103e6e29030e9a718e50ed37cf0
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-28 08:15:20 -0400

    Added relevant files and folders for Lab09.

---
+++ L09/EnrollmentApplication/EnrollmentApplication/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.Entity;
+using EnrollmentApplication.Models;
+
+namespace EnrollmentApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //commented out for Data Migration
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EnrollmentDB>());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
