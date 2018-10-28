24bae35a07cd366480461db2622dfc8151b8320d
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-28 10:12:32 -0400

    Added hide/show functionality.

--- L09/jQueryTestApplication/jQueryTestApplication/Scripts/Apps/TestScript.js
+++ L09/jQueryTestApplication/jQueryTestApplication/Scripts/Apps/TestScript.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function(){
+    $("#hide_btn").on("click", function () {
+        $(".hide_content").hide();
+    });
+
+    $("#show_btn").on("click", function () {
+        $(".hide_content").show();
+    });
+});
