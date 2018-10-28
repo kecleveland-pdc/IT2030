b03c9087f5402c1349a388435672c4353533f7cf
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-10-28 10:41:01 -0400

    Added styles and toggle functionality.

--- L09/jQueryTestApplication/jQueryTestApplication/Scripts/Apps/TestScript.js
+++ L09/jQueryTestApplication/jQueryTestApplication/Scripts/Apps/TestScript.js
@@ -6,4 +6,9 @@
     $("#show_btn").on("click", function () {
         $(".hide_content").show();
     });
+
+    $("#red_btn").on("click", function () {
+        $("#mainJQ").children("p").toggleClass("red important");
+        $("#mainJQ").find("a").css({ "color": "purple" });
+    })
 });
