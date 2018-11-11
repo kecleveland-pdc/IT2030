8004b6e5e4c0981b9643107ba57033f540b87f56
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-11-11 13:00:37 -0500

    Updated ShoppingCartController Index to handle session variable and return ShoppingCartViewModel. Added ShoppingCart class for Cart items. Updated db context with Carts.

--- L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/ShoppingCartController.cs
+++ L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/ShoppingCartController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCMusicStoreApplication.Models.ViewModels;
+using MVCMusicStoreApplication.Models;
 
 namespace MVCMusicStoreApplication.Controllers
 {
@@ -12,14 +13,24 @@ namespace MVCMusicStoreApplication.Controllers
         // GET: ShoppingCart
         public ActionResult Index()
         {
-            return View();
+            ShoppingCart cart = ShoppingCart.GetCart(this.HttpContext); // session variable
+            ShoppingCartViewModel vm = new ShoppingCartViewModel()
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetCartTotal()
+            };
+
+            return View(vm);
         }
 
+        //GET: /ShoppingCart/AddToCart/5
         public ActionResult AddToCart()
         {
             return View();
         }
 
+        //Ajax Call
+        //POST: /ShoppingCart/RemoveFromCart/5
         [HttpPost]
         public ActionResult RemoveFromCart()
         {
---
+++ L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Models/ShoppingCart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStoreApplication.Models
+{
+    public class ShoppingCart
+    {
+        public string ShoppingCartId;
+        private const string CartSessionKey = "CartId";
+        MVCMusicStoreDB db = new MVCMusicStoreDB(); //datacontext
+
+        public static ShoppingCart GetCart(HttpContextBase context)
+        {
+            ShoppingCart cart = new ShoppingCart();
+            cart.ShoppingCartId = cart.GetCartId(context);
+            return cart;
+        }
+
+        public string GetCartId(HttpContextBase context)
+        {
+            string cartId;
+            if(context.Session[CartSessionKey] == null)
+            {
+                //create a cartID and add it to session variable
+                cartId = Guid.NewGuid().ToString();
+                context.Session[CartSessionKey] = cartId;
+
+            }
+            else
+            {
+                //retrieve cartID
+                cartId = context.Session[CartSessionKey].ToString();
+            }
+
+            return cartId;
+        }
+
+        public List<Cart> GetCartItems()
+        {
+            return db.Carts.Where(cart => cart.CartID == ShoppingCartId).ToList();
+        }
+
+        public decimal GetCartTotal()
+        {
+            decimal? total = (from cartItems in db.Carts
+                             where cartItems.CartID == ShoppingCartId
+                             select cartItems.AlbumSelected.Price * (int?) cartItems.Count).Sum();
+            return total ?? decimal.Zero; 
+        }
+    }
+}
