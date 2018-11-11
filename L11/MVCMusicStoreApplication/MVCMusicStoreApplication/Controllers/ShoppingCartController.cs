d37378336a58ae06f6ee13135e73d9959c3917f2
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-11-11 12:11:20 -0500

    Added Cart, ShoppingCartController, and ShoppingCartViewModel.

---
+++ L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/ShoppingCartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models.ViewModels;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddToCart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart()
+        {
+            return View();
+        }
+    }
+}
