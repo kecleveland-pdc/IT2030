c5cf394d83896f65d8dd8ebfb318c31b50c22d11
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-12-02 20:10:10 -0500

    Added additional project and moved breaking project to new folder Failed_Project. Updated OWIN nuget references.

---
+++ Individual_Auth_Facebook/Current_Project/FaceBook_Auth_Project_II/FaceBook_Auth_Project_II/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FaceBook_Auth_Project_II.Startup))]
+namespace FaceBook_Auth_Project_II
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
