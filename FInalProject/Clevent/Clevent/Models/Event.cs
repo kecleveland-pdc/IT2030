62665d58857cb85934b0814f3677651e0261a2d9
KEIGH\Keigh <ke.cleveland@gmail.com>
2018-12-07 00:01:39 -0500

    Added Event, EventCart, EventYpe, Organizer classes.

---
+++ FInalProject/Clevent/Clevent/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Clevent.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxTickets { get; set; }
+        public int AvailableTickets { get; set; }
+
+        [Required]
+        public int ZipCode { get; set; }
+    }
+}
---
+++ FInalProject/Clevent/Clevent/Models/EventCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clevent.Models
+{
+    public class EventCart
+    {
+        public int EventCartID { get; set; }
+        public int Count { get; set; }
+        public int DateCreated { get; set; }
+        public Event EventItem { get; set; }
+        public Guid OrderNumber { get; set; }
+        public Event EventID { get; set; }
+    }
+}
